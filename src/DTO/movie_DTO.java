4b7631f18bffa6c82edff24cae0f20e8e7f28d32
Administrator <Administrator@WIN-JCT78BHHOI0>
2020-09-03 15:01:01 +0900

    0903프로젝트생성

---
+++ src/DTO/movie_DTO.java
@@ -0,0 +1,47 @@
+package DTO;
+
+public class movie_DTO {
+	private int no;
+	private int k_no;
+	private String name;
+	private String info;
+	private String r_time;
+	private int price;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getK_no() {
+		return k_no;
+	}
+	public void setK_no(int k_no) {
+		this.k_no = k_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getR_time() {
+		return r_time;
+	}
+	public void setR_time(String r_time) {
+		this.r_time = r_time;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
